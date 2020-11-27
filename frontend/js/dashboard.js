4a00d525dd7002cf53c77e697df26810948c9c2c
Kovaxs <oleksandr.kovalyk8@gmail.com>
2020-11-27 13:46:54 +0100

    Frontend template finished

--- frontend/js/dashboard.js
+++ frontend/js/dashboard.js
@@ -6,9 +6,54 @@
   feather.replace()
 
   // Graphs
-  var ctx = document.getElementById('myChart')
+  var presence_ctx = document.getElementById('presence')
+  var teperature_ctx = document.getElementById('temperature')
+  var humidity_ctx = document.getElementById('humidity')
   // eslint-disable-next-line no-unused-vars
-  var myChart = new Chart(ctx, {
+  var presenceChart = new Chart(presence_ctx, {
+    type: 'bar',
+    data: {
+      labels: [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+      ],
+      datasets: [{
+        data: [
+          15339,
+          21345,
+          18483,
+          24003,
+          23489,
+          24092,
+          12034
+        ],
+        lineTension: 0,
+        backgroundColor: 'transparent',
+        borderColor: '#007bff',
+        borderWidth: 5,
+        pointBackgroundColor: '#007bff'
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: false
+          }
+        }]
+      },
+      legend: {
+        display: false
+      }
+    }
+  })
+
+  var teperatureChart = new Chart(teperature_ctx, {
     type: 'line',
     data: {
       labels: [
@@ -33,7 +78,50 @@
         lineTension: 0,
         backgroundColor: 'transparent',
         borderColor: '#007bff',
-        borderWidth: 4,
+        borderWidth: 5,
+        pointBackgroundColor: '#007bff'
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: false
+          }
+        }]
+      },
+      legend: {
+        display: false
+      }
+    }
+  })
+
+  var humidityChart = new Chart(humidity_ctx, {
+    type: 'line',
+    data: {
+      labels: [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+      ],
+      datasets: [{
+        data: [
+          15339,
+          21345,
+          18483,
+          24003,
+          23489,
+          24092,
+          12034
+        ],
+        lineTension: 0,
+        backgroundColor: 'transparent',
+        borderColor: '#007bff',
+        borderWidth: 5,
         pointBackgroundColor: '#007bff'
       }]
     },
